7c7dec5d1a68d767672af277c0f4fdf83da5f74a
zurfyx <zurfyx@gmail.com>
2016-03-24 10:31:42 +0100

    template get users

--- lotube/users/mixins.py
+++ lotube/users/mixins.py
@@ -6,3 +6,9 @@ from .models import User
 class UserListMixin(ListView):
     model = User
     template_name = 'users/user_list.html'
+
+    def get_queryset(self):
+        a = User.objects.all()
+        print a[0].user.username
+        print a
+        return a
