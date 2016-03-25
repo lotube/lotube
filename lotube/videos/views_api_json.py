ec46820d94e04d507a309b56d1fcb3d31a085b3f
zurfyx <zurfyx@gmail.com>
2016-03-25 18:42:12 +0100

    abstraction of items in videos json views

--- lotube/videos/views_api_json.py
+++ lotube/videos/views_api_json.py
@@ -3,23 +3,27 @@ from core.mixins import JSONView
 from .mixins import VideoListMixin, VideoDetailMixin, VideoUserListMixin
 
 
+def _get_item(db_video):
+    return {
+        'id': db_video.id,
+        'source_id': db_video.id,
+        'source': constants.PROJECT_NAME,
+        'user': db_video.user.username,
+        'title': db_video.title,
+        'description': db_video.description,
+        'created_at': db_video.created,
+        'modified_at': db_video.modified,
+        'video_filename': db_video.filename,
+    }
+
+
 class VideoListJSON(JSONView, VideoListMixin):
     """
     List of Videos
     """
 
     def craft_response(self, context, **response_kwargs):
-        items = [{
-            'id': db_video.id,
-            'source_id': db_video.id,
-            'source': constants.PROJECT_NAME,
-            'user': db_video.user.username,
-            'title': db_video.title,
-            'description': db_video.description,
-            'created_at': db_video.created,
-            'modified_at': db_video.modified,
-            'video_filename': db_video.filename,
-        } for db_video in context['video_list']]
+        items = [_get_item(db_video) for db_video in context['video_list']]
         response = {
             'page_info': {
                 'total_results': len(items),
@@ -37,17 +41,7 @@ class VideoDetailJSON(JSONView, VideoDetailMixin):
 
     def craft_response(self, context, **response_kwargs):
         db_video = context['object']
-        response = {
-            'id': db_video.user_id,
-            'source_id': db_video.user_id,
-            'source': constants.PROJECT_NAME,
-            'user': db_video.user.username,
-            'title': db_video.title,
-            'description': db_video.description,
-            'created_at': db_video.created,
-            'modified_at': db_video.modified,
-            'video_filename': db_video.filename,
-        }
+        response = _get_item(db_video)
         return response
 
 
@@ -57,17 +51,7 @@ class VideoUserListJSON(JSONView, VideoUserListMixin):
     """
 
     def craft_response(self, context, **response_kwargs):
-        items = [{
-            'id': db_video.id,
-            'source_id': db_video.id,
-            'source': constants.PROJECT_NAME,
-            'user': db_video.user.username,
-            'title': db_video.title,
-            'description': db_video.description,
-            'created_at': db_video.created,
-            'modified_at': db_video.modified,
-            'video_filename': db_video.filename,
-        } for db_video in context['video_list']]
+        items = [_get_item(db_video) for db_video in context['video_list']]
         response = {
             'page_info': {
                 'total_results': len(items),
