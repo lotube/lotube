8801e571df4ff08bf28d1d1508a8c5cba21dcd0a
Eduard Torres <etorres404@gmail.com>
2016-03-25 18:46:16 +0100

    Factorized users/views_api_json. Added new migration

--- lotube/users/views_api_json.py
+++ lotube/users/views_api_json.py
@@ -2,22 +2,25 @@ from core.mixins import JSONView
 from .mixins import UserListMixin, UserDetailMixin
 
 
+def get_item(db_user):
+    return {
+        'id': db_user.id,
+        'username': db_user.username,
+        'first_name': db_user.first_name,
+        'last_name': db_user.last_name,
+        'created_at': db_user.date_joined,
+        'last_login': db_user.last_login,
+        'is_staff': db_user.is_staff,
+        'is_active': db_user.is_active,
+    }
+
 class UserListJSON(JSONView, UserListMixin):
     """
     List of users
     """
 
     def craft_response(self, context, **response_kwargs):
-        items = [{
-            'id': db_user.id,
-            'username': db_user.username,
-            'first_name': db_user.first_name,
-            'last_name': db_user.last_name,
-            'created_at': db_user.date_joined,
-            'last_login': db_user.last_login,
-            'is_staff': db_user.is_staff,
-            'is_active': db_user.is_active,
-        } for db_user in context['user_list']]
+        items = [get_item(db_user) for db_user in context['user_list']]
         response = {
             'page_info': {
                 'total_results': len(items),
@@ -35,14 +38,4 @@ class UserDetailJSON(JSONView, UserDetailMixin):
 
     def craft_response(self, context, **response_kwargs):
         user = context['object']
-        response = {
-            'id': user.id,
-            'username': user.username,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'created_at': user.date_joined,
-            'last_login': user.last_login,
-            'is_staff': user.is_staff,
-            'is_active': user.is_active,
-        }
-        return response
+        return get_item(user)
