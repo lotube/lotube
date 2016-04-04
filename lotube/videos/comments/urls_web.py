0c5bffc7db510e0b4b3c749c764cab261c4cab80
zurfyx <zurfyx@gmail.com>
2016-04-05 00:42:45 +0200

    html comments can no longer be viewed individually from html

--- lotube/videos/comments/urls_web.py
+++ lotube/videos/comments/urls_web.py
@@ -5,16 +5,16 @@ from .views_web import CommentList, CommentDetail
 
 urlpatterns = [
     # List of Comments
-    url(
-        r'^$',
-        CommentList.as_view(),
-        name='comments'
-    ),
+    #url(
+    #    r'^$',
+    #    CommentList.as_view(),
+    #    name='comments'
+    #),
 
     # Retrieve a specific Comment by id
-    url(
-        r'^\/(?P<pk>\d+)$',
-        CommentDetail.as_view(),
-        name='comment'
-    ),
+    #url(
+    #    r'^\/(?P<pk>\d+)$',
+    #    CommentDetail.as_view(),
+    #    name='comment'
+    #),
 ]
