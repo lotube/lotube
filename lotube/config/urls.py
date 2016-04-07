828ad4a73ad1d536d513b4a22b57bffaa526dc7e
zurfyx <zurfyx@gmail.com>
2016-04-07 12:18:40 +0200

    base url redirect to /videos

--- lotube/config/urls.py
+++ lotube/config/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^bot/', include('bot.urls')),
     url(r'^api/v1/', include('config.urls_api', namespace='api')),
     url(r'^', include('config.urls_web', namespace='web')),
+    url(r'^', RedirectView.as_view(url='videos'))
 ]
