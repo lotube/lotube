b3a4cca304add24b7ef869475b98b93597124a26
zurfyx <zurfyx@gmail.com>
2016-04-15 23:19:55 +0200

    user models tests

--- lotube/users/test_models.py
+++ lotube/users/test_models.py
@@ -1,12 +1,90 @@
+from datetime import timedelta
 from django.test import TestCase
 
+from users import constants
 from users.models import User
 
 
 class TestDefaultUser(TestCase):
 
+    INACTIVE_AFTER = constants.ACTIVE_DAYS  # days
+
     def setUp(self):
-        self.user = User()
+        super(TestDefaultUser, self).setUp()
+        self.user = User(
+            username='default',
+            email='default@example.com',
+        )
+        self.user.set_password('password')
+        self.user.save()
+
+    def test_is_active_after_creation(self):
+        self.assertTrue(self.user.is_active)
+
+    def test_is_active_after_30_days_creation(self):
+        self.user.date_joined = self.user.date_joined + \
+                                timedelta(days=-self.INACTIVE_AFTER)
+        self.user.save()
+        self.assertTrue(self.user.is_active)
+
+    def test_is_active_after_30_days_login(self):
+        # last login is still null (never logged in) -> we pick date_joined
+        self.user.last_login = self.user.date_joined + \
+                                timedelta(days=-self.INACTIVE_AFTER)
+        self.user.save()
+        self.assertTrue(self.user.is_active)
+
+    def test_is_active_after_31_days_creation_30_days_login(self):
+        self.user.date_joined = self.user.date_joined + \
+                                timedelta(days=-self.INACTIVE_AFTER+1)
+        # last login is still null (never logged in) -> we pick date_joined
+        self.user.last_login = self.user.date_joined + \
+                               timedelta(days=-self.INACTIVE_AFTER)
+        self.user.save()
+        self.assertTrue(self.user.is_active)
+
+    def test_is_not_active_after_31_days_creation(self):
+        self.user.date_joined = self.user.date_joined + \
+                                timedelta(days=-self.INACTIVE_AFTER-1)
+        self.user.save()
+        self.assertFalse(self.user.is_active)
+
+    def test_is_not_active_after_31_days_creation_31_days_login(self):
+        self.user.date_joined = self.user.date_joined + \
+                                timedelta(days=-self.INACTIVE_AFTER-1)
+        # last login is still null (never logged in) -> we pick date_joined
+        self.user.last_login = self.user.date_joined + \
+                               timedelta(days=-self.INACTIVE_AFTER-1)
+        self.user.save()
+        self.assertFalse(self.user.is_active)
+
+    def test_is_not_superuser(self):
+        self.assertFalse(self.user.is_superuser)
 
     def test_is_not_staff(self):
         self.assertFalse(self.user.is_staff)
+
+    def test_short_name_is_first_name(self):
+        pass
+
+    def test_full_name_is_first_name_and_last_name(self):
+        pass
+
+
+class TestAdminUser(TestCase):
+
+    def setUp(self):
+        super(TestAdminUser, self).setUp()
+        self.user = User(
+            username='admin',
+            email='admin@example.com',
+            is_staff=True,
+        )
+        self.user.set_password('password')
+        self.user.save()
+
+    def test_is_superuser(self):
+        self.assertTrue(self.user.is_superuser)
+
+    def test_is_staff(self):
+        self.assertTrue(self.user.is_staff)
